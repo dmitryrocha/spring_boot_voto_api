a1c4b571ba1124889030d10f172cb4061d60e756
Dmitry Rocha e Silva <dmitry.silva@solutis.net.br>
2022-04-24 16:39:29 -0300

    Initial commit

---
+++ src/main/java/br/com/solutis/voto_api/voto_api/VotoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.solutis.voto_api.voto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solutis/voto_api/voto_api/VotoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.voto_api.voto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
