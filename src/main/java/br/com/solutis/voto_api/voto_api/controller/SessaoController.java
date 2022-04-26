bf5c274b2c0338db730b32e6a96597e70647e307
Dmitry Rocha <dmitryrocha@gmail.com>
2022-04-26 19:58:30 -0300

    Sessions are started and agendas are having their status changed. It still needs to close automatically

--- src/main/java/br/com/solutis/voto_api/voto_api/controller/SessaoController.java
+++ src/main/java/br/com/solutis/voto_api/voto_api/controller/SessaoController.java
@@ -2,12 +2,17 @@ package br.com.solutis.voto_api.voto_api.controller;
 
 import br.com.solutis.voto_api.voto_api.controller.dto.SessaoDto;
 import br.com.solutis.voto_api.voto_api.model.Sessao;
+import br.com.solutis.voto_api.voto_api.model.SessaoForm;
+import br.com.solutis.voto_api.voto_api.repository.PautaRepository;
 import br.com.solutis.voto_api.voto_api.repository.SessaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController @RequestMapping("/sessao")
@@ -16,9 +21,23 @@ public class SessaoController {
     @Autowired
     private SessaoRepository sessaoRepository;
 
+    @Autowired
+    private PautaRepository pautaRepository;
+
     @GetMapping
     public List<SessaoDto> listar() {
         List<Sessao> sessoes = sessaoRepository.findAll();
         return SessaoDto.converter(sessoes);
+    }
+
+    @PostMapping({"/iniciar"})
+    @Transactional
+    public ResponseEntity<SessaoDto> iniciar(@RequestBody @Valid SessaoForm sessaoForm, UriComponentsBuilder uriComponentsBuilder) {
+        Sessao sessao = sessaoForm.converter(pautaRepository);
+        sessaoRepository.save(sessao);
+
+        URI uri = uriComponentsBuilder.path("/sessao/{id}").buildAndExpand(sessao.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new SessaoDto(sessao));
     }
 }
