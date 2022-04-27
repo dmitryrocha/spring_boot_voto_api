4ae240ad227c0ff9eee89aece0ca6f65bf230ce3
Dmitry Rocha <dmitryrocha@gmail.com>
2022-04-27 11:12:32 -0300

    API documentation added using Swagger and SpringFox

--- src/main/java/br/com/solutis/voto_api/voto_api/controller/PautaController.java
+++ src/main/java/br/com/solutis/voto_api/voto_api/controller/PautaController.java
@@ -4,6 +4,9 @@ import br.com.solutis.voto_api.voto_api.controller.dto.PautaDto;
 import br.com.solutis.voto_api.voto_api.model.Pauta;
 import br.com.solutis.voto_api.voto_api.model.PautaForm;
 import br.com.solutis.voto_api.voto_api.repository.PautaRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +23,29 @@ public class PautaController {
     @Autowired
     private PautaRepository pautaRepository;
 
+    @ApiOperation(value = "Retorna uma lista de pautas cadastradas")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna a lista de pautas"),
+            @ApiResponse(code = 401, message = "Você não tem autorização para ver a lista"),
+            @ApiResponse(code = 403, message = "Você está proibido de ver a lista"),
+            @ApiResponse(code = 404, message = "Recurso não encontrado"),
+
+    })
     @GetMapping
     public List<PautaDto> listar() {
         List<Pauta> pautas = pautaRepository.findAll();
         return PautaDto.converter(pautas);
     }
 
+    @ApiOperation(value = "Cadastra uma pauta")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 201, message = "Cadastro criado com sucesso"),
+            @ApiResponse(code = 401, message = "Você não tem autorização realizar cadastro"),
+            @ApiResponse(code = 403, message = "Você está proibido de realizar cadastro"),
+            @ApiResponse(code = 404, message = "Recurso não encontrado"),
+
+    })
     @PostMapping @Transactional
     public ResponseEntity<PautaDto> cadastrar(@RequestBody @Valid PautaForm pautaForm, UriComponentsBuilder uriBuilder) {
         Pauta pauta = pautaForm.converter();
