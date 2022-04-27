180112a82e35b1fff06beecc317ab22aaa79bc5d
Dmitry Rocha <dmitryrocha@gmail.com>
2022-04-27 12:09:09 -0300

    Associate registration with CPF validation added

--- src/main/java/br/com/solutis/voto_api/voto_api/controller/AssociadoController.java
+++ src/main/java/br/com/solutis/voto_api/voto_api/controller/AssociadoController.java
@@ -2,15 +2,19 @@ package br.com.solutis.voto_api.voto_api.controller;
 
 import br.com.solutis.voto_api.voto_api.controller.dto.AssociadoDto;
 import br.com.solutis.voto_api.voto_api.model.Associado;
+import br.com.solutis.voto_api.voto_api.model.AssociadoForm;
 import br.com.solutis.voto_api.voto_api.repository.AssociadoRepository;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 
@@ -33,4 +37,26 @@ public class AssociadoController {
         List<Associado> associados = associadoRepository.findAll();
         return AssociadoDto.converter(associados);
     }
+
+    @ApiOperation(value = "Cadastra um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 201, message = "Cadastro criado com sucesso"),
+            @ApiResponse(code = 401, message = "Você não tem autorização realizar cadastro"),
+            @ApiResponse(code = 403, message = "Você está proibido de realizar cadastro"),
+            @ApiResponse(code = 404, message = "Recurso não encontrado"),
+
+    })
+    @PostMapping
+    @Transactional
+    public ResponseEntity<AssociadoDto> cadastrar(@RequestBody @Valid AssociadoForm associadoForm, UriComponentsBuilder uriBuilder) {
+        Associado associado = associadoForm.converter();
+        associadoRepository.save(associado);
+
+        URI uri = uriBuilder.path("/associado/{id}").buildAndExpand(associado.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new AssociadoDto(associado));
+    }
+
+
 }
